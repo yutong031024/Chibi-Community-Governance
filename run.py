36ea02a8232b1fe77c649dd7ef0f7618f2aa6254
yutong031024 <luo.yutong031024@gmail.com>
2026-07-09 22:19:32 +0800

    v0.2 Initialize Flask backend

--- run.py
+++ run.py
@@ -1,1 +1,13 @@
-print("Chibi Community Governance Platform")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Welcome to Chibi Community Governance Platform!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
